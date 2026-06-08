0d22d65b2bc6f1deb87cc12440d0560d6bbbfbe5
iseungsoo07 <iseungsoo07@gmail.com>
2026-06-08 13:07:38 +0900

    Initial Commit

---
+++ src/main/java/com/woodugserver/WoodugServerApplication.java
@@ -0,0 +1,13 @@
+package com.woodugserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodugServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoodugServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woodugserver/WoodugServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woodugserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodugServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
